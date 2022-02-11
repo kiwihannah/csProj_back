fd3b221dc64b4539ac0a5b39f65e302097daebd6
Hannah <gkssk8827@gmail.com>
2022-02-11 15:57:29 +0900

    server open testing

---
+++ app.js
@@ -0,0 +1,33 @@
+'user strict';
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("3조 서버 오픈 테스트");
+});
+
+app.use("/api", bodyParser.json(), router);
+// app.use(express.static("./assets"));
+
+const myLogger = function (req, res, next) {
+  console.log(
+    new Date().toLocaleTimeString(),
+    "| Request URL:",
+    req.originalUrl
+  );
+  next();
+};
+app.use(myLogger);
+
+app.listen(port, () => {
+  console.log(
+    new Date().toLocaleTimeString(),
+    "|",
+    port,
+    ": server has been connected"
+  );
+});
