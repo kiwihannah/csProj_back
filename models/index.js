470e5b2bf6a20acbd11707fc032d3707debb5d60
fancyers <kjng5901@gmail.com>
2022-02-15 00:38:01 +0900

    db url을 환경변수로 저장

--- models/index.js
+++ models/index.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connect = () => {
     mongoose
-        .connect(
-            'mongodb://test:test@3.35.169.150:27017/cheat-sheet?authSource=admin',
-            { ignoreUndefined: true }
-        ) 
+        .connect(process.env.DB_URL, { ignoreUndefined: true })
         .catch((err) => {
-            console.log(err);
-        })    
+            console.log(err)
+        })
 }
 
 module.exports = connect
