82d7858da5c2e52ea0b0e85d75ab1fdb81636884
fancyers <kjng5901@gmail.com>
2022-02-15 14:09:58 +0900

    주석에 DB 주소 삭제

--- models/index.js
+++ models/index.js
@@ -3,14 +3,7 @@ require('dotenv').config()
 
 const connect = () => {
   mongoose
-    // .connect(
-    //   'mongodb://test:test@3.35.169.150:27017/cheat-sheet?authSource=admin',
-    //   { ignoreUndefined: true }
-    // )
-    .connect(
-        process.env.DB_URL,
-        { ignoreUndefined: true }
-      )
+    .connect(process.env.DB_URL, { ignoreUndefined: true })
     .catch((err) => {
       console.log(err)
     })
