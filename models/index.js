c5bebc62fe0b6470e8abdfccbd62daa0f7fb9fde
fancyers <kjng5901@gmail.com>
2022-02-15 10:43:02 +0900

    환경 변수 테스트 2

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
-// require('dotenv').config()
+require('dotenv').config()
 
 const connect = () => {
   mongoose
-    .connect(
-      'mongodb://test:test@3.35.169.150:27017/cheat-sheet?authSource=admin',
-      { ignoreUndefined: true }
-    )
     // .connect(
-    //     process.env.DB_URL,
-    //     { ignoreUndefined: true }
-    //   )
+    //   'mongodb://test:test@3.35.169.150:27017/cheat-sheet?authSource=admin',
+    //   { ignoreUndefined: true }
+    // )
+    .connect(
+        process.env.DB_URL,
+        { ignoreUndefined: true }
+      )
     .catch((err) => {
       console.log(err)
     })
