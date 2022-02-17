c21be241ca592e7d44a1e1458c6520f49ad76cf5
fancyers <kjng5901@gmail.com>
2022-02-17 22:57:42 +0900

    프로필 사진 업로드 기능 주석 처리 없앰

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const usersRouter = require('./routes/users')
 const questionsRouter = require('./routes/questions')
 const answersRouter = require('./routes/answers')
 const likesRouter = require('./routes/likes')
-//const profileImagesRouter = require('./routes/profile-image')
+const profileImagesRouter = require('./routes/profile-image')
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs))
 
 app.use(
